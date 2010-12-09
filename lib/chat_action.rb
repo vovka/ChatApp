d63c15e635a1717737f256c64dff60d1c12243cb
vovka <sch.icq@gmail.com>
2010-12-09 12:17:18 +0200

    hardcoded pseudo devise auth

--- lib/chat_action.rb
+++ lib/chat_action.rb
@@ -2,24 +2,27 @@ require 'active_support/inflector'
 require 'active_record'
 
 class ChatAction < Cramp::Websocket
-  include AbstractController::Helpers
-  #include Devise::Controllers::Helpers
 
   on_start :started
   on_data :received_data
   
   def received_data(data)
-    msg = "Got your #{data}"
-    #render msg
-    session = @env['rack.session']
-    klass, key = session['warden.user.user.key']
-    klass = klass.constantize
-    user = klass.find_by_id key
-    #puts "current_user: #{current_user.inspect}"
-    puts "Hello, #{user.email}!"
+    puts "Hello, #{current_user.email}!"
+    msg = "#{current_user.email}: #{data}"
+    render msg
   end
 
   def started
     puts "\n\n\nstarted\n\n\n"
   end
+
+  private
+
+    def current_user
+      return @current_user if @current_user
+      session = @env['rack.session']
+      klass, key = session['warden.user.user.key']
+      klass = klass.constantize
+      @current_user = klass.find_by_id key
+    end
 end
