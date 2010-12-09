3ed45ff4208f4c323a13992391bbe51994663505
vovka <sch.icq@gmail.com>
2010-12-09 13:06:30 +0200

    simple chat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101130184858) do
+ActiveRecord::Schema.define(:version => 20101209102849) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "body"
+    t.integer  "author_id"
+    t.integer  "recipient_id"
+    t.boolean  "read",         :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
--- lib/chat_action.rb
+++ lib/chat_action.rb
@@ -5,11 +5,15 @@ class ChatAction < Cramp::Websocket
 
   on_start :started
   on_data :received_data
+  periodic_timer :pull_chat_messages, :every => 1
   
-  def received_data(data)
-    puts "Hello, #{current_user.email}!"
-    msg = "#{current_user.email}: #{data}"
-    render msg
+  def received_data data
+    if signed_in?
+      recipient = not_me
+      msg = Message.create :author_id => current_user.id, :recipient_id => recipient.id,
+        :body => data, :read => false
+      render formatted(msg)
+    end
   end
 
   def started
@@ -22,7 +26,34 @@ class ChatAction < Cramp::Websocket
       return @current_user if @current_user
       session = @env['rack.session']
       klass, key = session['warden.user.user.key']
-      klass = klass.constantize
-      @current_user = klass.find_by_id key
+      unless klass.nil?
+        klass = klass.constantize
+        @current_user = klass.find_by_id key
+      else
+      end
+    end
+
+    def signed_in?
+      not current_user.nil?
+    end
+
+    def pull_chat_messages
+      if signed_in?
+        messages = Message.unread(current_user)
+        if messages.any?
+          messages.each do |m|
+            render formatted(m)
+            m.update_attribute :read, true
+          end
+        end
+      end
+    end
+
+    def formatted message
+      "<strong>#{message.author.email}:</strong>&nbsp;#{message.body}"
+    end
+
+    def not_me
+      @not_me ||= User.where(["NOT id = ?", current_user.id]).first
     end
 end
